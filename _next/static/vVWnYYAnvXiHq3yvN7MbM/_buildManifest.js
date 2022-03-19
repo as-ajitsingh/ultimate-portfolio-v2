60fe37899b8613f8b11940109f3f655bcd21a9a2
as-ajitsingh <as.ajitsingh@outlook.com>
2022-03-19 20:29:22 +0000

    Deploying to gh-pages from @ as-ajitsingh/ultimate-portfolio-v2@91f281526af654dd2e02648fcc60ae9ad92a69a7 🚀

--- _next/static/vVWnYYAnvXiHq3yvN7MbM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a4079ab371b91900e50e.css","static/chunks/pages/index-9bc549e6d4337aa76deb.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
