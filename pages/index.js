a4fe979f503914e3ea91f255fb8ddeae07bbd64c
Ajit Singh <as.ajitsingh@outlook.com>
2022-03-20 20:20:14 +0530

    Added blog section

--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,39 @@ import Head from 'next/head'
 import Terminal from '../components/terminal';
 import ProjectCard from '../components/project-card';
 import styles from '../styles/Home.module.css'
-import {getAllProjects} from "./api/projects-service";
+import { getAllProjects } from "./api/projects-service";
+import { getAllBlogs } from './api/blogs-service';
 // import SkillsChart from "../components/skills-chart/skills-chart";
 import ContactForm from "../components/contact-form/contact-form";
+import BlogCard from '../components/blog-card';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+    const [blogs, setBlogs] = useState([]);
+    useEffect(() => {
+        getAllBlogs().then(setBlogs).catch(console.error);
+    }, []);
     return (
         <>
             <nav className={styles.navbar}>
-                <img src='images/logo.svg' alt="Ajit Singh logo" className={styles.logo}/>
+                <img src='images/logo.svg' alt="Ajit Singh logo" className={styles.logo} />
             </nav>
             <div id="main" className={styles['first-container']}>
                 <Head>
                     <title>Ajit Singh</title>
-                    <link rel="icon" href="images/favicon.ico"/>
+                    <link rel="icon" href="images/favicon.ico" />
                     {/* <base href={`/${packageJson.name}/`}/> */}
                 </Head>
                 <main className={styles.main}>
                     <h1 className={styles.heading}>Hi, my name is Ajit.</h1>
                     <p className={styles.subtext}>Wanted to know more, <a href="#chatbot">click here</a></p>
                 </main>
-                <img className={styles.waves} src='images/waves2.svg' alt='waves-border-image'/>
+                <img className={styles.waves} src='images/waves2.svg' alt='waves-border-image' />
             </div>
             <div id="chatbot" className={styles['second-container']}>
                 <section className={styles['chatbot-section']}>
                     <h2 className={styles['chatbot-heading']}>ask the chatbot.</h2>
-                    <Terminal/>
+                    <Terminal />
                 </section>
             </div>
             <div id="project-list" className={styles['third-container']}>
@@ -35,6 +42,14 @@ export default function Home() {
                     <h2 className={styles['section-heading']}>personal projects</h2>
                     <section className={styles['project-cards-section']}>
                         {getAllProjects().map(ProjectCard)}
+                    </section>
+                </section>
+            </div>
+            <div id="blogs" className={styles['fourth-container']}>
+                <section className={styles['blogs-section']}>
+                    <h2 className={styles['section-heading']}>some blogs</h2>
+                    <section className={styles['blog-cards-section']}>
+                        {blogs.map(BlogCard)}
                     </section>
                 </section>
             </div>
@@ -46,7 +61,7 @@ export default function Home() {
             </div> */}
             <div id="contact" className={styles['fifth-container']}>
                 <section className={styles['contact-section']}>
-                    <ContactForm/>
+                    <ContactForm />
                 </section>
             </div>
         </>
