59d3c376f71f37149d28d58c77e090d667c4cada
as-ajitsingh <as.ajitsingh@outlook.com>
2022-03-20 14:56:40 +0000

    Deploying to gh-pages from @ as-ajitsingh/ultimate-portfolio-v2@a4fe979f503914e3ea91f255fb8ddeae07bbd64c 🚀

---
+++ _next/static/J_Jpiic4o21UySQZgCdz6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/520-14de7bea30d0ee94.js","static/css/89ad0e4197e3178e.css","static/chunks/pages/index-f235b35d8cc7b2f8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/XQvWryRbYerjZgqUF_qzE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/765-58a9061b2467b508324b.js","static/css/36b4f8156cde78b88dd6.css","static/chunks/pages/index-875b6e392c38f30451da.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
