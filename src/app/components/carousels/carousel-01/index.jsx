2d1f5d0bcf09b4ecaee9369004af76b4b7688abd
Ajit Singh <as.ajitsingh@outlook.com>
2025-05-10 13:42:03 +0530

    adds carousel slider pause

--- src/app/components/carousels/carousel-01/index.jsx
+++ src/app/components/carousels/carousel-01/index.jsx
@@ -1,6 +1,7 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Card from "../../card";
+import { FaPlay } from "react-icons/fa";
 
 const Carousel01 = ({}) => {
   const projects = [
@@ -13,7 +14,7 @@ const Carousel01 = ({}) => {
   ];
 
   const pageLength = 4;
-  
+
   const projectPages = projects.reduce((pages, project, index) => {
     pages[Math.floor(index / pageLength)] =
       pages[Math.floor(index / pageLength)] ?? [];
@@ -21,36 +22,44 @@ const Carousel01 = ({}) => {
     return pages;
   }, new Array(Math.floor(projects.length / pageLength)).fill([]));
 
-  const [selectPage, setSelectedPage] = useState(0);
+  const sliderTimerRef = useRef();
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const [sliderPaused, setSliderPaused] = useState(false);
 
   useEffect(() => {
     const sliderDuration = 3_000;
+    if (!sliderPaused)
+      sliderTimerRef.current = setInterval(() => {
+        setCurrentPage((currentPage) =>
+          currentPage == projectPages.length - 1 ? 0 : currentPage + 1
+        );
+      }, sliderDuration);
 
-    const sliderTimer = setInterval(() => {
-      console.log(selectPage);
-      setSelectedPage((selectPage) =>
-        selectPage == projectPages.length - 1 ? 0 : selectPage + 1
-      );
-    }, sliderDuration);
-
-    return () => clearInterval(sliderTimer);
-  }, []);
+    return () => clearInterval(sliderTimerRef.current);
+  }, [sliderPaused]);
 
   return (
     <>
       <div className="h-140 flex items-center relative bg-secondary-700 px-20 rounded-md">
         <div className="grid grid-cols-2 gap-6">
-          {projectPages[selectPage].map((title, idx) => (
+          {projectPages[currentPage].map((title, idx) => (
             <Card key={idx} title={title} />
           ))}
         </div>
-        <div className="absolute right-4 top-1/2 transform -translate-y-1/2 flex flex-col gap-2">
+        <div className="absolute right-4 top-1/2 transform -translate-y-1/2 flex flex-col gap-4 items-center">
+          <FaPlay
+            onClick={() => setSliderPaused((sliderPaused) => !sliderPaused)}
+            className={`w-3 h-3  ${
+              sliderPaused ? "text-primary-100" : "text-accent-300"
+            }`}
+          />
           {projectPages.map((_, i) => (
             <div
-              onClick={() => setSelectedPage(i)}
+              onClick={() => setCurrentPage(i)}
               key={i}
               className={`w-3 h-3 rounded-full ${
-                i === selectPage ? "bg-blue-500" : "bg-gray-400"
+                i === currentPage ? "bg-accent-300" : "bg-primary-100"
               }`}
             ></div>
           ))}
