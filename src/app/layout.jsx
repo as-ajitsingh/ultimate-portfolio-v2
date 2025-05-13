f6f139043fa14296ee0b055bda357fe3658c7c7e
Ajit Singh <as.ajitsingh@outlook.com>
2025-05-14 00:52:40 +0530

    updated to use optimized font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,14 +4,10 @@ import Navbar from "../components/navbar";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import Link from "next/link";
+import Head from "next/head";
+import { Nunito } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const nunito = Nunito({
   subsets: ["latin"],
 });
 
@@ -21,7 +17,7 @@ export const metadata = {
   icons: {
     icon: "/favicon.svg",
     shortcut: "/favicon.svg",
-  }
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -35,46 +31,44 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`antialiased`}>
         <Navbar websiteTitle="Portfolio" links={links} />
         <section className="max-w-[75rem] mx-auto">{children}</section>
         <section
           id="footer"
           className="h-100 bg-secondary-700 flex flex-row items-center justify-around"
         >
-            <div id="left" className="flex flex-col gap-1">
-              <h1 className="flex text-3xl md:text-5xl justify-center md:justify-start">
-                Contact
-              </h1>
-              <span className="block p-1 text-lg text-primary-100 dark:text-secondary-300 font-normal w-72">
-                Feel free to connect with me via any of these mediums.
-              </span>
-            </div>
-            <div id="right" className="flex gap-16">
-              <Link
-                href="https://x.com/__azt"
-                target="_blank"
-                alt="Social Media Twitter"
-              >
-                <FaXTwitter className="w-5 h-5" />
-              </Link>
-              <Link
-                href="https://github.com/as-ajitsingh"
-                target="_blank"
-                alt="Social Media GitHub"
-              >
-                <FaGithub className="w-5 h-5" />
-              </Link>
-              <Link
-                href="https://www.linkedin.com/in/as-ajitsingh/"
-                target="_blank"
-                alt="Social Media LinkedIn"
-              >
-                <FaLinkedin className="w-5 h-5" />
-              </Link>
-            </div>
+          <div id="left" className="flex flex-col gap-1">
+            <h1 className="flex text-3xl md:text-5xl justify-center md:justify-start">
+              Contact
+            </h1>
+            <span className="block p-1 text-lg text-primary-100 dark:text-secondary-300 font-normal w-72">
+              Feel free to connect with me via any of these mediums.
+            </span>
+          </div>
+          <div id="right" className="flex gap-16">
+            <Link
+              href="https://x.com/__azt"
+              target="_blank"
+              alt="Social Media Twitter"
+            >
+              <FaXTwitter className="w-5 h-5" />
+            </Link>
+            <Link
+              href="https://github.com/as-ajitsingh"
+              target="_blank"
+              alt="Social Media GitHub"
+            >
+              <FaGithub className="w-5 h-5" />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/as-ajitsingh/"
+              target="_blank"
+              alt="Social Media LinkedIn"
+            >
+              <FaLinkedin className="w-5 h-5" />
+            </Link>
+          </div>
         </section>
       </body>
     </html>
